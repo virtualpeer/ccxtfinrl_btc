23921874e35e3e1a9b689eb6977da8236b3bc0db
rayrui312 <61444295+rayrui312@users.noreply.github.com>
2021-04-10 23:20:42 +0800

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from finrl.config import config
+from finrl.preprocessing.preprocessors import FeatureEngineer
+
+# local csv to df
+df = pd.read_csv('btc_usdt_1m.csv', names=['date','open','high',
+                                           'low','close','volume'])
+# insert cryptocurrency type
+df.insert(loc=1, column='tic',value='btc')
+print(df[0:5])
+
+
+fe = FeatureEngineer(
+                    use_technical_indicator=True,
+                    tech_indicator_list = config.TECHNICAL_INDICATORS_LIST,
+                    use_turbulence=True,
+                    user_defined_feature = False)
+        
+processed = fe.preprocess_data(df)
+print(processed[0:10])
+processed.to_csv('processed_t.csv')
